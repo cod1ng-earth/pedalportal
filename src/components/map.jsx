036d647c77141b693643d23c12b56b688c46d16e
susott <37674058+susott@users.noreply.github.com>
2018-09-15 15:03:55 +0200

    simple map is working

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
 
+console.log(LeafletMap);
+
 class Map extends Component {
-  state = {
-    lat: 52.520,
-    lng: 3.405,
-    zoom: 13,
+  constructor (props) {
+    super(props);
+    this.state = {
+      lat: 52.520,
+      lng: 13.405,
+      zoom: 12,
+    }
   }
+
 
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
-      <div id="mapid">
-        <LeafletMap center={position} zoom={this.state.zoom}>
+        <LeafletMap center={position} zoom={this.state.zoom} id="mapid">
           <TileLayer
             attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
         </LeafletMap>
-      </div>
     );
   }
 }
