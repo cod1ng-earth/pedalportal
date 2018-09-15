13f89a525aaa9cf6ce8004d3f42d94c82a3fc879
susott <37674058+susott@users.noreply.github.com>
2018-09-15 17:06:59 +0200

    Map component3 (#4)
    * zwei marker
    
    * rendering markers dynamically from Sheet.json

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
-
-console.log(LeafletMap);
+import Sheet from '../demodata/sheet.json';
 
 class Map extends Component {
   constructor (props) {
@@ -12,21 +11,25 @@ class Map extends Component {
       zoom: 12,
     }
   }
-
-
   render() {
     const position = [this.state.lat, this.state.lng]
+    const markers = Sheet.results.map((el, index) => (
+      <Marker position={el.location} key={index}>
+          <Popup>
+            {el.name} <br />
+            {el.tags.map((tag) =>
+              `${tag}, `
+            )}
+          </Popup>
+        </Marker>
+      ));
     return (
       <LeafletMap center={position} zoom={this.state.zoom} id="mapid">
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            Yeah. <br /> This is my first marker.
-          </Popup>
-        </Marker>
+        {markers}
       </LeafletMap>
     );
   }
